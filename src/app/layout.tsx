d0dade0d0f931b2394d1f37cebfb768dee04d42e
Alfej FF <alfejff117@gmail.com>
2025-09-14 17:47:13 +0000

    Or kya kya kar sak te hai

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,9 +32,6 @@ export default function RootLayout({
       {isFirebaseEnabled && <AnalyticsProvider />}
       <head>
         <link rel="manifest" href="/manifest.json" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased flex flex-col min-h-screen bg-background">
         <Header />
